4ebf69d07866fb5bb241e722188eb9b552be4fab
Subham Sahoo <subham01rkl@gmail.com>
2025-04-21 01:29:10 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -71,6 +71,15 @@ CREATE TABLE bronze.erp_loc_a101(
 
 );
 GO
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+)
+GO
 
 
 
